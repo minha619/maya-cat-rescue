b219c5834000a33843eb96a9a8f2ea595fc8e33c
minha619 <minha619@gmail.com>
2023-11-15 15:34:36 -0500

    componets added

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+function About() {
+
+    return (
+      <section className="my-5" >
+        <h2 id="about"> About Maya Cats and Kittens Rescue </h2>
+        <div className="my-2">
+       
+
+        </div>
+      </section>
+    )
+  }
+export default About;
+
