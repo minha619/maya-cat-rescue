cd25ecac4d98bf6a5dfe2bf0585ca2ecbdb22eea
minha619 <minha619@gmail.com>
2023-11-16 21:07:39 -0500

    logo adjust

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,10 @@
 function About() {
 
     return (
-      <section className="my-5" >
-        <h2 id="about"> About Maya Cats and Kittens Rescue </h2>
-        <div className="my-2">
+      <section >
+        <div>
+        <h1> About Maya Cats and Kittens Rescue </h1>
+
        
 
         </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,14 +4,16 @@ function Nav() {
     return (
         <header>
             <nav>
-                <div>
-                    <img src={require(`../../assets/img/maya-logo.pngç`)} alt="logo"/>
+                <div className="flex flex-row">
+                    <div className="px-4 py-4">
+                        <img className="w-48" src={require(`../../assets/img/maya-logo.png`)} alt="logo" />
+                    </div>
+                    <ul className="text-orange-300 flex flex-row">
+                        <li className="px-4 py-4"> <Link to='home/'> HOME </Link> </li>
+                        <li className="px-4 py-4"> <Link to='about/'> ABOUT US </Link> </li>
+                        <li className="px-4 py-4"> <Link to='about/'> ADOPT </Link> </li>
+                    </ul>
                 </div>
-               <ul>
-                    <li> <Link to='home/'> HOME </Link> </li>
-                    <li> <Link to='about/'> ABOUT US </Link> </li>
-                    <li> <Link to='about/'> ADOPT </Link> </li>
-                </ul>
             </nav>
         </header>
 
