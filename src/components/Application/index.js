732308cd10ccea3b2bd0873b89bb509b18d19145
minha619 <minha619@gmail.com>
2024-01-15 12:26:31 -0500

    pdf form update

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -2,12 +2,16 @@
 
 function Application() {
 
+    const onClick = () => {
+        window.open('../../assets/Cat-Adoption-Application-Form.pdf');
+    }
+
     return (
         <section className="h-full mt-2 px-4 py-10 bg-bgyellow opacity-80 text-center font-sans">
             <h1 className="font-bold text-2xl"> How to apply </h1>
             <p className="pt-6">Please download and fill out a pdf form and send it to mayacatsrescue@gmail.com</p>
             {/* <Link to="../../assets/Cat-Adoption-Application-Form.pdf/" target="_blank"> */}
-            <button type="submit" onclick="window.open('../../assets/Cat-Adoption-Application-Form.pdf')" className="rounded-full bg-hoverwhite text-bgyellow px-4 py-2 mt-6 font-bold">  DOWNLOAD FORM </button>
+            <button type="submit" onClick={onClick} className="rounded-full bg-hoverwhite text-bgyellow px-4 py-2 mt-6 font-bold">DOWNLOAD FORM </button>
             {/* </Link> */}
         </section>
     )
