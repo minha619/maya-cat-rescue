a4109ee53b21114b0ae670c4002a4f9588514107
minha619 <minha619@gmail.com>
2024-02-16 18:42:13 -0500

    venmoid

--- src/components/Donate/index.js
+++ src/components/Donate/index.js
@@ -2,16 +2,27 @@ function Donate() {
 
     return (
         <section className="h-full mt-2 px-6 py-10 bg-bgyellow opacity-80 text-center font-sans" >
-            <h1 className="font-bold text-2xl"> Make Donation to Maya Cats and Kittens Rescue </h1>
-            <p className="px-32 py-10">
-                Founded in 2020, Maya Cats and Kittens Rescue,Inc. is a 501(c)(3) nonprofit organization.
-            </p>
-            <p>
-                <h1 className="font-bold">Zelle ID: </h1> MAYA CATS AND KITTENS RESCUE, INC, SOYSSET, NY
-                <br /><br />
-                <h1 className="font-bold">Venmo ID:</h1> @alee03
-            </p>
-        </section>
+            <div>
+                <h1 className="font-bold text-2xl"> Make Donation to Maya Cats and Kittens Rescue </h1>
+                <p className="px-32 py-10">
+                    Founded in 2020, Maya Cats and Kittens Rescue,Inc. is a 501(c)(3) nonprofit organization.
+                </p>
+                <p>
+                    <h1 className="font-bold">Zelle ID:
+                    </h1>
+                    516-650-9462 <br />
+                    MAYA CATS AND KITTENS RESCUE, INC, SOYSSET, NY <br /><br />
+
+                    <h1 className="font-bold">Venmo ID:</h1> @Misook380
+                </p>
+            </div>
+            <div className="flex justify-center">
+                <div className="w-60 mt-6">
+                    <img src={require(`../../assets/img/venmoid.jpg`)} alt="venmoid" />
+                </div>
+            </div>
+
+        </section >
     )
 }
 export default Donate;
